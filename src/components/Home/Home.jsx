e65461899961a1ecfd63df052b47ccd51129df70
Sharia2 <sarisromanpa@gmail.com>
2020-11-26 00:16:59 -0500

    resultados titulo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ import { RightOutlined, LeftOutlined} from '@ant-design/icons'
 import image1 from "../../assets/1.png"
 import image2 from "../../assets/2.png"
 import image3 from "../../assets/3.png"
-import image4 from "../../assets/4.jpg"
+import image4 from "../../assets/4.png"
 
 
 const images = [image1, image2, image3, image4];
